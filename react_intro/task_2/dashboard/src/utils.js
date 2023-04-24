efc670c7b111748a582ab9acbc52a1935e5b1174
coder-akash <akashd@brightcomgroup.com>
2023-04-23 23:17:46 -0700

    task 2 modification

--- react_intro/task_2/dashboard/src/utils.js
+++ react_intro/task_2/dashboard/src/utils.js
@@ -8,3 +8,6 @@ export const getFooterCopy = (isIndex) =>(
     isIndex ?'Holberton School':'Holberton School main dashboard'
   )
 
+export const getLatestNotification = () =>(
+    '<strong>Urgent requirement</strong> - complete by EOD'
+)
