c9c86caf89dd8e42bb2660d36169a4f22f589ad1
coder-akash <akashd@brightcomgroup.com>
2023-04-23 23:08:37 -0700

    task 3

---
+++ react_intro/task_2/dashboard/src/Notifications.js
@@ -0,0 +1,13 @@
+import './Notifications.css'
+import {getLatestNotification} from './utils'
+import React from 'react'
+
+const Notifications = () =>{
+    return(
+        <div className='Notifications'>
+            <p>Here is the list of notifications</p>
+        </div>
+    )
+
+}
+export default Notifications
