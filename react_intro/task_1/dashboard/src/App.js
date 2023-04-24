14952aed30d030f3520bce5cc351264878e5d439
coder-akash <akashd@brightcomgroup.com>
2023-04-23 23:02:41 -0700

    task 1

---
+++ react_intro/task_1/dashboard/src/App.js
@@ -0,0 +1,22 @@
+import './App.css'
+import logo from './logo.jpg';
+import {getFullYear, getFooterCopy} from './utils'
+
+const App = () =>{
+  return(
+    <>
+    <div className='App-header'>
+      <img src={logo} alt='img'></img>
+      <h1>School dashboard</h1>
+    </div>
+    <div className='App-body'>
+      <p>Login to access the full dashboard</p>
+
+    </div>
+    <div className='App-footer'>
+      <p>>Copyright {getFullYear()} - {getFooterCopy(true)}</p>
+    </div>
+    </>
+  )
+}
+export default App
